d5408dd2923d40e44e49726c1ee6572492d238a5
Bivekich <biveki-dev@yandex.ru>
2024-08-12 00:28:21 +0300

    build: header

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return <div className="app">
+    <Header />
+  </div>;
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="header">
+      <Container className="header-container">
+        <Navbar.Brand href="#home" className="brand">
+          КАСМЕТ
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto"> {/* Центрируем ссылки */}
+            <Nav.Link href="#about">О компании</Nav.Link>
+            <Nav.Link href="#catalog">Каталог</Nav.Link>
+            <Nav.Link href="#world">В мире</Nav.Link>
+            <Nav.Link href="#clients">Заказчики</Nav.Link>
+            <Nav.Link href="#addresses">Адреса</Nav.Link>
+          </Nav>
+          <Button variant="outline-light" className="contact-button">Контакты</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
